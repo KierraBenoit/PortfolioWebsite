1995e264b38e8873819131cee18f3515424051aa
KierraBenoit <kierrabenoitk@yahoo.com>
2023-08-10 12:02:52 -0500

    form js

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ const navmenu = document.getElementsByClassName('nav-menu')[0]
 toggleButton.addEventListener('click', () => {
   navmenu.classList.toggle('active')
 })
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".contact-form");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+    const name = form.querySelector("#name").value;
+    const email = form.querySelector("#email").value;
+    const message = form.querySelector("#message-area").value;
+
+    if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
+      alert("Please fill in all fields.");
+      return;
+    }
+    form.reset();
+
+    alert("Message sent!");
+  });
+});
